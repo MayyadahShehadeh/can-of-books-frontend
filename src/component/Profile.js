10e224bec714ac0986eb3e52b3b57f014941e33d
mayyadah <miss.mayadah1@gmail.com>
2021-07-04 17:16:57 +0300

    edit components

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,29 +1,23 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { withAuth0 } from "@auth0/auth0-react";
 
+export class Profile extends Component {
+  render() {
+      return (
+          <div>
+              {
+                  this.props.auth0.isAuthenticated&&
+                  <>
+                  <h1>{this.props.auth0.user.name}</h1>
+                  <img src={this.props.auth0.user.picture} alt={this.props.auth0.user.name}/>
+                  </>
 
-
-
-
-
-
-
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
+              }
+              
+          </div>
+      )
   }
+}
 
-  return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  );
-};
 
-export default Profile;
+export default withAuth0(Profile);
