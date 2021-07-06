23b64d4846fe65bc342b33589cfbc7370f7de401
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-07-06 17:38:38 +0300

    done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -5,45 +5,72 @@ import './BestBooks.css';
 import axios from 'axios';
 import Book from './component/Book';
 
-const serverUrl = process.env.REACT_APP_SERVER_URL;
+// const serverUrl = process.env.PORT;
 
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
     super(props)
-    
-    this.state = {
-        userEmail: this.props.auth0.user.email,
-        booksData: []
-    }
-}
 
-componentDidMount = async () => {
-  await axios.get(`${serverUrl}/books?email=${this.state.userEmail}`).then(response => {
-           this.setState({
-          booksData: response.data[response.data.length-1].books,
+    this.state = {
+      userEmail: '',
+      booksData: []
+    }
+  }
+
+  // componentDidMount = async () => {
+  //   await axios.get(`${serverUrl}/books?email=${this.state.userEmail}`).then(response => {
+  //            this.setState({
+  //           booksData: response.data[response.data.length-1].books,
+  //       })
+  //   }).catch(error => alert(error))
+  // }
+
+  inputEmail = (e) => {
+    this.setState({
+      userEmail: e.target.value
+    })
+
+
+  }
+  onClickSearch = (e) => {
+    e.preventDefault();
+
+    axios.get('http://localhost:8000/books?email=bardaweel95.rawan@gmail.com').then(response => {
+      this.setState({
+        booksData: response.data
+        
       })
-  }).catch(error => alert(error))
-}
+      
+      console.log(response.data)
+    })
+
+  }
 
   render() {
-    return(
+    return (
       <>
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-      <div>
-                {
-                    this.state.booksData.length > 0 &&
-                    <Book
-                        booksData={this.state.booksData}
-                    />
-                }
-            </div>
- 
+      {console.log('100')}
+        <Jumbotron>
+          <h1>My Favorite Books</h1>
+          <form >
+            <input type="text" placeholder="Email" onChange={(e) => { this.inputEmail(e) }} />
+            <button onClick={(e) => { this.onClickSearch(e) }} > Search </button>
+
+          </form>
+          <p>
+            This is a collection of my favorite books
+          </p>
+        </Jumbotron>
+        <div>
+          {/* {console.log('hiii',booksData)} */}
+          
+          {this.state.booksData.map(book => {
+            return <li><Book bookName={book.name} /></li>
+          })
+          }
+
+        </div>
       </>
     )
   }
